b0ba851723c88d022e0beb4c95d8243fe9cdbe6d
Valeriy <walera.ru11@mail.ru>
2022-08-03 19:49:02 +0300

    27

---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.WriteLine("Введите значение: ");
+int n = int.Parse(Console.ReadLine());
+int summa = 0;
+while (n != 0)
+{
+    int x = n % 10;
+    summa = summa + x;
+    n = n / 10;
+}
+Console.WriteLine(summa);
