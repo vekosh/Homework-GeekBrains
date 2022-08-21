67429b8a63cd8a9ca533acdc825fb514bea7818f
Valeriy <walera.ru11@mail.ru>
2022-08-21 13:29:40 +0300

    64

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 5, 6, 7, 8""
+*/
+System.Console.Write("Введите требуемый промежуток  \nВведите через пробел: ");
+string[] input1 = Console.ReadLine().Split();
+int num1 = int.Parse(input1[0]);
+int num2 = int.Parse(input1[1]);
+GetSum(num1, num2);
+
+void GetSum(int n1, int n2)
+{
+    if (n1 > n2)
+    {
+        return;
+    }
+    Console.Write(n1 + " ");
+    GetSum(++n1, n2);
+}
